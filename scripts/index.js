ad6aa1ac72c7c67825c684e52fd9ca62fab6ddb0
Aleksandra Babicheva <alex-elm@yandex.ru>
2021-02-21 00:10:52 +0300

    исправления после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,33 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let captionInput = formElement.querySelector('.popup__input_type_caption');
 
+let nameProfile = document.querySelector('.profile__name');
+let caption = document.querySelector('.profile__caption');
 
-function popupVisibility() {
-  if (popup.classList.contains('popup_opened') === false) {
-    popup.classList.add('popup_opened');
-  } else {
-    popup.classList.remove('popup_opened');
-  }
+function popupOpen() {
+    popup.classList.toggle('popup_opened');
+
+  nameInput.value =  nameProfile.textContent;
+  captionInput.value = caption.textContent;
+}
+
+function popupClose() {
+  popup.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let nameProfile = document.querySelector('.profile__name');
-    let caption = document.querySelector('.profile__caption');
-
     nameProfile.textContent = nameInput.value;
     caption.textContent = captionInput.value;
+
+    popup.classList.toggle('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-editButton.addEventListener('click', popupVisibility);
-closeButton.addEventListener('click', popupVisibility);
-submitButton.addEventListener('click', popupVisibility);
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+
 
 
